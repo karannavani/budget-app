13e13bce6bc7b6ed429d3dea7e81e219b7c6bfec
Louis Glick <mrldglick@gmail.com>
2018-08-16 18:19:33 +0100

    seeds file user and goals done, still got expenses to do

---
+++ db/seeds.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const { dbURI } = require('../config/environment');
+const User = require('../models/user');
+const Goal = require('../models/goal');
+const Expense = require('../models/expense');
+
+mongoose.connect(dbURI);
+Goal.collection.drop();
+User.collection.drop();
+
+const userData = [
+  { firstName: 'Louis', lastName: 'Glick', email: 'lg@hotmail.com', password: 'pass', passwordConfirmation: 'pass' },
+  { firstName: 'Tristan', lastName: 'Hall', email: 'th@hotmail.com', password: 'pass', passwordConfirmation: 'pass' },
+  { firstName: 'Karan', lastName: 'Navani', email: 'kn@hotmail.com', password: 'pass', passwordConfirmation: 'pass' }
+];
+
+const goalData = [{
+  name: 'Vespa SS 180',
+  cost: 1200,
+  deadline: '24/03/2019',
+  alreadySaved: 200,
+  imageUrl: 'http://th03.deviantart.net/fs70/PRE/f/2012/140/0/1/flcl_haruko__s_vespa_super_sport_180_by_boomerjinks-d50h9gc.jpg'
+}, {
+  name: 'Apple Watch',
+  cost: 250,
+  deadline: '25/12/2018',
+  alreadySaved: 7,
+  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBdZDW8urlFatOLD7pfYaSNFVlwoQxpDLLvnc0nS8xvoQCo2n59g',
+  websiteUrl: 'https://www.apple.com/uk/shop/buy-watch/apple-watch'
+}, {
+  name: 'Fulgurite',
+  cost: 200,
+  deadline: 'N/A',
+  alreadySaved: 15,
+  imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51cNQCKiOXL._UY625_.jpg',
+  websiteUrl: 'http://www.minresco.com/fulgurites/fulgurites.htm'
+}];
+
+
+// // TODO: In need of equilvalent.
+//     |
+//     |
+//     V
+// User
+//   .create(userData)
+//   .then(users => {
+//     console.log(`Created ${users.length} users.`);
+//     whiskeyData[0].createdBy = users[0].id;
+//     whiskeyData[1].createdBy = users[0].id;
+//     return Whiskey.create(whiskeyData);
+//   })
+//   .then(whiskeys => {
+//     console.log(`Created ${whiskeys.length} whiskeys.`);
+//   })
+//   .catch(err => console.log(err))
+//   .finally(() => mongoose.connection.close());
