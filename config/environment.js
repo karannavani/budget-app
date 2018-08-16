21ed3a66444d7482eb672807b1240cd78b4c7644
Karan Navani <kknavani@gmail.com>
2018-08-16 20:16:49 +0100

    set up testing folders and env config

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 4000;
-const dbURI = process.env.DB_URI || 'mongodb://localhost/thrifty';
+const env = process.env.NODE_ENV || 'dev'; //production or development
+const dbURI = process.env.DB_URI || `mongodb://localhost/thrifty-${env}`;
 const secret = process.env.SECRET || 'wdi35';
 
 
