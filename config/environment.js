56543535b339f1606170bf59b5e08c85c275c917
Tristan Hall <tristanalex1986@gmail.com>
2018-08-16 18:27:53 +0100

    environment file updated

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,6 @@
 const port = process.env.PORT || 4000;
+const dbURI = process.env.DB_URI || 'mongodb://localhost/thrifty';
+const secret = process.env.SECRET || 'wdi35';
 
-module.exports = { port };
+
+module.exports = { dbURI, port, secret };
