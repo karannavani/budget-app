07888908f8bff46ee2f5c48cc58937c97bb38f3f
Karan Navani <kknavani@gmail.com>
2018-08-17 15:23:59 +0100

    completed user profile edit

--- src/controllers/profile/edit.js
+++ src/controllers/profile/edit.js
@@ -1,5 +1,19 @@
-function ProfileEditCtrl() {
+function ProfileEditCtrl($scope, $http, $state) {
+  $scope.updateProfile = function() {
+    $http({
+      method: 'PUT',
+      url: `api/profile/${$state.params.id}`,
+      data: $scope.user
+    })
+      .then(() => $state.go('profileShow'), { id: $state.params.id });
+  };
 
+
+  $http({
+    method: 'GET',
+    url: `api/profile/${$state.params.id}`
+  })
+    .then(res => $scope.user = res.data);
 }
 
 export default ProfileEditCtrl;
