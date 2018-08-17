7ed7261df1262ae4c318a15bd72107ae509fbd3a
Karan Navani <kknavani@gmail.com>
2018-08-17 15:47:09 +0100

    added Louis' changes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ import AuthRegisterCtrl from './controllers/auth/register';
 import ProfileShowCtrl from './controllers/profile/show';
 import ProfileEditCtrl from './controllers/profile/edit';
 
+import GoalsIndexCtrl from './controllers/goals/index';
+import GoalsShowCtrl from './controllers/goals/show';
+import GoalsEditCtrl from './controllers/goals/edit';
+import GoalsNewCtrl from './controllers/goals/new';
+
+
 angular.module('Thrifty',
   [ 'ui.router', 'satellizer', 'ngMessages' ]
 )
@@ -20,6 +26,10 @@ angular.module('Thrifty',
   .controller('AuthRegisterCtrl', AuthRegisterCtrl)
   .controller('ProfileShowCtrl', ProfileShowCtrl)
   .controller('ProfileEditCtrl', ProfileEditCtrl)
+  .controller('GoalsIndexCtrl', GoalsIndexCtrl)
+  .controller('GoalsShowCtrl', GoalsShowCtrl)
+  .controller('GoalsEditCtrl', GoalsEditCtrl)
+  .controller('GoalsNewCtrl', GoalsNewCtrl)
   .config(Router)
   .config(function($authProvider) {
     $authProvider.loginUrl = '/api/login';
