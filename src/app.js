4d1f6c4225cc07d30241201eb402d2ecb815ec14
Louis Glick <mrldglick@gmail.com>
2018-08-17 16:04:47 +0100

    fixed Louis

--- src/app.js
+++ src/app.js
@@ -13,10 +13,6 @@ import AuthRegisterCtrl from './controllers/auth/register';
 import ProfileShowCtrl from './controllers/profile/show';
 import ProfileEditCtrl from './controllers/profile/edit';
 
-import GoalsIndexCtrl from './controllers/goals/index';
-import GoalsShowCtrl from './controllers/goals/show';
-import GoalsEditCtrl from './controllers/goals/edit';
-import GoalsNewCtrl from './controllers/goals/new';
 import ExpensesIndexCtrl from './controllers/expenses/index';
 import ExpensesShowCtrl from './controllers/expenses/show';
 import ExpensesEditCtrl from './controllers/expenses/edit';
@@ -36,10 +32,7 @@ angular.module('Thrifty',
   .controller('AuthRegisterCtrl', AuthRegisterCtrl)
   .controller('ProfileShowCtrl', ProfileShowCtrl)
   .controller('ProfileEditCtrl', ProfileEditCtrl)
-  .controller('GoalsIndexCtrl', GoalsIndexCtrl)
-  .controller('GoalsShowCtrl', GoalsShowCtrl)
-  .controller('GoalsEditCtrl', GoalsEditCtrl)
-  .controller('GoalsNewCtrl', GoalsNewCtrl)
+  
   .controller('ExpensesIndexCtrl', ExpensesIndexCtrl)
   .controller('ExpensesShowCtrl', ExpensesShowCtrl)
   .controller('ExpensesEditCtrl', ExpensesEditCtrl)
@@ -49,7 +42,7 @@ angular.module('Thrifty',
   .controller('GoalsShowCtrl', GoalsShowCtrl)
   .controller('GoalsEditCtrl', GoalsEditCtrl)
   .controller('GoalsNewCtrl', GoalsNewCtrl)
-  
+
   .config(Router)
   .config(function($authProvider) {
     $authProvider.loginUrl = '/api/login';
