e6bb7c518f823179832a3b4c338a8bfae7c16c96
Karan Navani <kknavani@gmail.com>
2018-08-17 15:04:18 +0100

    created auth registration

--- src/app.js
+++ src/app.js
@@ -9,11 +9,13 @@ import Router from './config/routes';
 
 // Controllers
 import AuthLoginCtrl from './controllers/auth/login';
+import AuthRegisterCtrl from './controllers/auth/register';
 
 angular.module('Thrifty',
   [ 'ui.router', 'satellizer', 'ngMessages' ]
 )
   .controller('AuthLoginCtrl', AuthLoginCtrl)
+  .controller('AuthRegisterCtrl', AuthRegisterCtrl)
   .config(Router)
   .config(function($authProvider) {
     $authProvider.loginUrl = '/api/login';
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,18 +1,28 @@
 function Router($stateProvider, $urlRouterProvider) {
 
-// function secureState($q, $state, $auth, $rootScope) {
-//   return new $q((resolve) => {
-//     if ($auth.isAuthenticated()) return resolve();
-//     console.log('Creating a flash message from the router');
-//
-//   })
-// }
+  // function secureState($q, $state, $auth, $rootScope) {
+  //   return new $q((resolve) => {
+  //     if ($auth.isAuthenticated()) return resolve();
+  //     console.log('Creating a flash message from the router');
+  //     $rootScope.$broadcast('flashMessage', {
+  //       type: 'warning',
+  //       content: 'please log in'
+  //     });
+  //     $state.go('login');
+  //   });
+  // }
 
   $stateProvider
+
     .state('login', {
       templateUrl: './views/auth/login.html',
       url: '/login',
       controller: 'AuthLoginCtrl' //ADD THIS IN
+    })
+    .state('register', {
+      templateUrl: './views/auth/register.html',
+      url: '/register',
+      controller: 'AuthRegisterCtrl'
     });
 
   $urlRouterProvider.otherwise('/');
