28f0b4043f0e389ae9ff408bf0e67a4c810fc798
Karan Navani <kknavani@gmail.com>
2018-08-17 10:01:39 +0100

    made few updates to the config router

---
+++ src/config/routes.js
@@ -0,0 +1,13 @@
+function Router($stateProvider, $urlRouterProvider) {
+
+// function secureState($q, $state, $auth, $rootScope) {
+//   return new $q((resolve) => {
+//     if ($auth.isAuthenticated()) return resolve();
+//     console.log('Creating a flash message from the router');
+//
+//   })
+// }
+
+}
+
+export default Router;
