bb8ec78553a38d73053ce1a641483d1acc5436f8
Karan Navani <kknavani@gmail.com>
2018-08-21 21:02:49 +0100

    burger menu now closes automatically

--- src/external.js
+++ src/external.js
@@ -2,10 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const navbarBurgers = document.querySelector('.navbar-burger');
   const navbarMenu = document.querySelector('.navbar-menu');
+  const options = document.getElementsByClassName('navbar-item');
 
-  navbarBurgers.addEventListener('click', () => {
+  function toggleActive() {
     navbarBurgers.classList.toggle('is-active');
     navbarMenu.classList.toggle('is-active');
+  }
+
+  navbarBurgers.addEventListener('click', () => {
+    toggleActive();
   });
+
+  for (let i=0; i < options.length; i++ ) {
+    options[i].addEventListener('click', () => {
+      toggleActive();
+
+    });
+  }
+
 
 });
