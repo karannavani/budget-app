e132c4a5e6ecff2a519810f2c31b0d21e336aa30
Louis Glick <mrldglick@gmail.com>
2018-08-21 16:11:49 +0100

    uber api working, had problem with skipAuthorization but all sorted! Happy Day!!!

--- src/controllers/journey/index.js
+++ src/controllers/journey/index.js
@@ -8,11 +8,36 @@ function JourneyIndexCtrl($scope, $http) {
     $scope.lon = $scope.position.coords.longitude;
     console.log('lat is', $scope.lat);
     console.log('lon is', $scope.lon);
+    getUberPrices();
   });
 
-
-
+  function getUberPrices() {
+    $http({
+      method: 'GET',
+      url: 'https://api.uber.com/v1.2/estimates/price',
+      params: {
+        start_latitude: $scope.lat,
+        start_longitude: $scope.lon,
+        end_latitude: 51.53,
+        end_longitude: 0.07
+      },
+      headers: {
+        Authorization: 'Token dxlEpZmVHfdFFkOr7nZ3SAfjcJvRb081DnQM_2KS'
+      },
+      skipAuthorization: true
+    })
+      .then(res => {
+        $scope.priceUber = res.data;
+        console.log('priceUber is', $scope.priceUber);
+      })
+      .catch(err => console.log('An error with uber', err));
+  }
 }
+
+
+
+
+
 
 
 export default JourneyIndexCtrl;
