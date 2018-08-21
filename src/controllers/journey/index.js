0a05e71a2403266f60a9ea8f8d7e94297f65f979
Karan Navani <kknavani@gmail.com>
2018-08-21 16:19:00 +0100

    tfl data showing up in journey

--- src/controllers/journey/index.js
+++ src/controllers/journey/index.js
@@ -10,7 +10,36 @@ function JourneyIndexCtrl($scope, $http) {
     console.log('lon is', $scope.lon);
   });
 
+  $scope.$watch('endPoint', () => {
+    if($scope.endPoint) {
+      console.log('End point is', $scope.endPoint);
+    }
+  });
 
+  $scope.generateOptions = function() {
+
+    $http.get(`https://api.postcodes.io/postcodes/${$scope.endPoint}`)
+      .then(res => {
+        $scope.endLat = res.data.result.latitude;
+        $scope.endLon = res.data.result.longitude;
+        console.log('End Lat is', $scope.endLat);
+        console.log('End Lon is', $scope.endLon);
+        getTfl();
+      });
+
+    function getTfl() {
+      $http({
+        method: 'GET',
+        url: `https://api.tfl.gov.uk/Journey/JourneyResults/${$scope.lat}%2C${$scope.lon}/to/${$scope.endLat}%2C-${$scope.endLon}/?mode=tube`,
+        skipAuthorization: true
+      })
+        .then(res => {
+          console.log(res.data);
+        });
+    }
+
+
+  };
 
 }
 
