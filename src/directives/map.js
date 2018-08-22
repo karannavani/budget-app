1e2bc3821e7c581059aed90283d4fd97a0a63dca
Tristan Hall <tristanalex1986@gmail.com>
2018-08-22 23:23:03 +0100

    maps added and working, just needs tiles and import

--- src/directives/map.js
+++ src/directives/map.js
@@ -5,19 +5,21 @@ function Map() {
     link($scope, $element) {
       const DomElement = $element[0];
       const map = L.map(DomElement);
+      map.setView([51.4175195, -0.0724934], 70);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
       }).addTo(map);
-      
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          const makerLocation = [position.coords.latitude, position.coords.longitude];
-          const marker =L.marker(makerLocation)
-            .addTo(map);
-          marker.bindPopup('<p>You are here!</p>');
-        });
-      }
+
+
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(position => {
+      //     const makerLocation = [position.coords.latitude, position.coords.longitude];
+      //     const marker =L.marker(makerLocation)
+      //       .addTo(map);
+      //     marker.bindPopup('<p>You are here!</p>');
+      //   });
+      // }
     }};
 }
 export default Map;
